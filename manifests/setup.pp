67a117babb01e860ec46597febab9a03522158e1
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-10 15:36:17 +0100

    Changed boxen_user to luser

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,23 +1,23 @@
 # This class make sure pathogen is installed and the .vim directories exist
 class vim::setup {
-  file { ["/Users/${::boxen_user}/.vim",
-    	  "/Users/${::boxen_user}/.vim/autoload",
-          "/Users/${::boxen_user}/.vim/bundle"]:
+  file { ["/Users/${::luser}/.vim",
+    	  "/Users/${::luser}/.vim/autoload",
+          "/Users/${::luser}/.vim/bundle"]:
     ensure  => directory,
     recurse => true,
   }
 
-  repository { "/Users/${::boxen_user}/.vim/vim-pathogen":
+  repository { "/Users/${::luser}/.vim/vim-pathogen":
     source => 'tpope/vim-pathogen'
   }
 
-  file { "/Users/${::boxen_user}/.vim/autoload/pathogen.vim":
-    target  => "/Users/${::boxen_user}/.vim/vim-pathogen/autoload/pathogen.vim",
+  file { "/Users/${::luser}/.vim/autoload/pathogen.vim":
+    target  => "/Users/${::luser}/.vim/vim-pathogen/autoload/pathogen.vim",
     require => [
-      File["/Users/${::boxen_user}/.vim"],
-      File["/Users/${::boxen_user}/.vim/autoload"],
-      File["/Users/${::boxen_user}/.vim/bundle"],
-      Repository["/Users/${::boxen_user}/.vim/vim-pathogen"]
+      File["/Users/${::luser}/.vim"],
+      File["/Users/${::luser}/.vim/autoload"],
+      File["/Users/${::luser}/.vim/bundle"],
+      Repository["/Users/${::luser}/.vim/vim-pathogen"]
     ]
   }
 }
