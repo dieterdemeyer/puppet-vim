fe1c60604e14b4c06410c2a78c4fdb3ac2df5145
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-10 15:32:00 +0100

    Include instead of require

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,8 +1,8 @@
 # This class make sure pathogen is installed and the .vim directories exist
 class vim::setup {
   file { ["/Users/${::boxen_user}/.vim",
-    "/Users/${::boxen_user}/.vim/autoload",
-    "/Users/${::boxen_user}/.vim/bundle"]:
+    	  "/Users/${::boxen_user}/.vim/autoload",
+          "/Users/${::boxen_user}/.vim/bundle"]:
     ensure  => directory,
     recurse => true,
   }
